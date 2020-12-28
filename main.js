7831a5bbb645141b8b7acd73e82342f36be22727
BC <f.bouchami@almaviva.tn>
2020-12-28 11:35:28 +0100

    //add users to the tabe

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var faker = require('faker');
+const url =' https://randomuser.me/api/ ';
 /*var person = {
   firstName: "",
   lastName: "",
@@ -6,13 +6,71 @@ var faker = require('faker');
 };
 */
 var usersList = [];
-function addRandomUser(){
-    let person = new Object();
-    person.firstName = faker.name.firstName;
-    person.lastName = faker.name.lastName;
-    person.wealth = Math.floor(Math.random() * 1000000);
-    usersList.push(person);
+
+
+function handleErrors (res){
+    if(!res.ok){
+      throw error(res.status);
+    }
+    console.log(res);
+    return res;
+  }
+  
+  function printError (error){
+    console.log(error);
+  }
+
+function parseJSON (res){
+    return res.json();
+  }
+
+
+
+  function loadData(){
+    var table = document.getElementById("tableBody");
+    table.innerHTML = "";
+    usersList.forEach(user => {
+    var tr = document.createElement("tr");
+    var tdName = document.createElement("td");
+    tdName.innerHTML = user.firstName + " " + user.lastName ; 
+    var tdWealth = document.createElement("td");
+    tdWealth.innerHTML = "$ "+user.wealth;
+    tr.appendChild(tdName);
+    tr.appendChild(tdWealth);
+    table.appendChild(tr);
+    });
 }
+
+
+function addRandomUser(profile){
+    let person = new Object();
+    person.firstName = profile.results[0].name.first;
+    person.lastName = profile.results[0].name.last;
+    person.wealth = Math.floor(Math.random() * 1000000);
+    console.log("Person "+person.toString());
+    usersList.push(person);
+    console.log("List "+usersList.length);
+    loadData();
+
+}
+
+
+
+var btnAddUser = document.getElementById("addRandomUser");
+btnAddUser.addEventListener("click",function() {
+fetch(url)
+.then(handleErrors)
+.then(parseJSON)
+.then(addRandomUser)
+.catch(printError)
+});
+
+
+
+
+
+
+
 
 
 function addUser(){
@@ -43,4 +101,10 @@ function calculateEntierWealth(){
   return  usersList.reduce((a, b) => a + b, 0);
 }
 
+
+
 }
+
+
+
+
