fce251187ed9ed88b434dfb457b0faa3556ee165
BC <f.bouchami@almaviva.tn>
2020-12-28 09:36:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+/*var person = {
+  firstName: "",
+  lastName: "",
+  age: 0,
+  wealth: 0
+};
+*/
+
+var usersList = [];
+function addUser(){
+    let person = new Object();
+    person.firstName = document.getElementById("firstName");
+    person.lastName = document.getElementById("lastName");
+    person.wealth = document.getElementById("wealth");
+    person.age = document.getElementById("age");
+    usersList.push(person);
+}
+
+function showOnlyMillionaires(){
+    return usersList.filter(user => {
+        user.wealth>=1000000
+    });
+
+function doubleMoney(){
+    usersList.forEach(user => {
+        user.wealth = user.wealth*2;
+    });
+}
+
+
+function sortByrichest(){
+   usersList.sort((a, b) => (a.wealth > b.wealth) ? 1 : -1);
+}
+
+}
